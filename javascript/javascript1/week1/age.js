0f2f5d0844021894ba99ccd7b4c151275914758b
annajohngeorge <annajohngeorge@gmail.com>
2020-02-27 10:46:06 +0100

    implemented homework exercises

--- javascript/javascript1/week1/age.js
+++
@@ -1,4 +0,0 @@
-const yearOfBirth = 1989;
-const yearFuture = 2045;
-const age = yearFuture - yearOfBirth;
-console.log("You will be " + age + " years old in " + yearFuture);
--- javascript/javascript1/week1/name.js
+++
@@ -1,5 +0,0 @@
-const firstWords = ['easy','awesome','corporate','find','perfect','go','flex', 'dynamic','techie','cyber'];
-const secondWords =['ideas','vision','solutions','trends','digital','aid','chip','byte','query','fanatic'];
-const randomNumber = Math.floor(Math.random() * 10) + 0;
-const startupName =firstWords[randomNumber] + secondWords[randomNumber];
-console.log("The startup: " + startupName  +" contains " + startupName.length +" characters") ;
