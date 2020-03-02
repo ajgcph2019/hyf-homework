a44f7edb372771ef26e62031cd63143295c689a8
ajgcph2019 <50801233+ajgcph2019@users.noreply.github.com>
2020-03-02 22:52:23 +0100

    Updated house.js
    Changed variable declaration with keyword const for variables which will not be re declared

--- javascript/javascript1/week1/house.js
+++ javascript/javascript1/week1/house.js
@@ -1,27 +1,34 @@
-let firstHouseWidth = 8;
-let firstHouseDepth = 10;
-let firstHouseHeight = 10;
-let gardenSizeInM2 = 100;
+const firstHouseWidth = 8;
+const firstHouseDepth = 10;
+const firstHouseHeight = 10;
+const gardenSizeInM2 = 100;
 let volumeInMeters;
-let sellingPrice_Peter = 2500000;
-let sellingPrice_Julia = 1000000;
-let secondHouseWidth = 5;
-let secondHouseDepth = 11;
-let secondHouseHeight = 8;
-let secondGardenSizeInM2 = 70;
+const sellingPrice_Peter = 2500000;
+const sellingPrice_Julia = 1000000;
+const secondHouseWidth = 5;
+const secondHouseDepth = 11;
+const secondHouseHeight = 8;
+const secondGardenSizeInM2 = 70;
+const housePrice ;
+const secondHousePrice;
+
 volumeInMeters = firstHouseDepth * firstHouseWidth * firstHouseHeight ;
-let housePrice ;
+
 housePrice= volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300 ;
 console.log("Sale price of the first home is  " +housePrice);
-if(sellingPrice_Peter >housePrice  )
-console.log("Peter is paying " + sellingPrice_Peter +" which exceeds the actual cost");
+
+if(sellingPrice_Peter >housePrice  ){
+                                      console.log("Peter is paying " + sellingPrice_Peter +" which exceeds the actual cost");
+                                     }
+
 let secondVolumeinMeters=  secondHouseWidth *  secondHouseDepth * secondHouseHeight;
-let secondHousePrice;
 secondHousePrice = secondVolumeinMeters * 2.5 * 1000 + secondGardenSizeInM2 * 300 ;
 console.log("Second house calculated price is " +secondHousePrice + " but sale price is " + sellingPrice_Julia);
-if(secondHousePrice > sellingPrice_Julia)
-console.log("Julia is paying less for the  house");
+
+if(secondHousePrice > sellingPrice_Julia){
+                                            console.log("Julia is paying less for the  house");
+                                          }
 else
-console.log("Julia is paying more for the  house");
+                                            console.log("Julia is paying more for the  house");
 
 
