9b1fbaf71d943d87afa05db84ac31051fc637b65
ajgcph2019 <50801233+ajgcph2019@users.noreply.github.com>
2020-03-02 22:46:50 +0100

    Updated dog_age.js
    Implemented feedback to use camelcase and format for code review

--- javascript/javascript1/week1/dog_age.js
+++ javascript/javascript1/week1/dog_age.js
@@ -1,14 +1,16 @@
 const dogYearOfBirth = 1957;
 const dogYearFuture = 2027;
 console.log(dogYearFuture - dogYearOfBirth);
-const dogYear = 60;
-const dog_age= dogYearFuture - dogYearOfBirth;
-if (dog_age <=dogYear) { 
- shouldShowResultInDogYears = true;
-console.log("Your dog will be " + dogYear + " dog years old in " + dogYearFuture);
- }
-else{
-shouldShowResultInDogYears = false;
-console.log("Your dog will be " + dogYear + " human years old in " + dogYearFuture);
-}
+const dogYear = 7;
+const dogAge= dogYearFuture - dogYearOfBirth;
+let shouldShowResultInDogYears = true;
+
+if (dogAge <=dogYear) { 
+                         console.log("Your dog will be " + dogYear + " dog years old in " + dogYearFuture);
+                      }
+else                      {
+                            shouldShowResultInDogYears = false;
+                            console.log("Your dog will be " + dogYear + " human years old in " + dogYearFuture);
+                          }
+
 console.log(shouldShowResultInDogYears);
