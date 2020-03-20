0e5a191b40b603e263a2e7dcf90a3527704249aa
annajohngeorge <annajohngeorge@gmail.com>
2020-03-20 14:13:36 +0100

    Modified the program using only indexOf and no for loop

--- javascript/javascript1/week3/ItemRemoval.js
+++ javascript/javascript1/week3/ItemRemoval.js
@@ -1,11 +1,14 @@
 const names = ['Peter', 'Ahmad', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala'];
 const nameToRemove = 'Ahmad';
 
-for ( let i = 0 ; i < names.length ; i++) {
-    
-    if (names[i].includes(nameToRemove)){
-        names.splice(i ,1) ;
-    }
-}
 
-console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
+ const namesIndex = names.indexOf(nameToRemove) ;
+ //console.log(namesIndex); For reference
+
+ //console.log(names[namesIndex]);
+
+    if (names[namesIndex] === nameToRemove)  {
+        names.splice(namesIndex , 1);
+    }
+
+console.log(names);     // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
