3bf494107f1b44bfc6ecf1d887e43bf2626ebbe7
ajgcph2019 <annajohngeorge@gmail.com>
2020-06-24 21:48:56 +0200

    added changes as per feedback for week1 homework

--- nodejs/week1/warmup/circles.js
+++ nodejs/week1/warmup/circles.js
@@ -4,22 +4,24 @@ class Circle {
   constructor(radius) {
     this.radius = radius;
   }
-  getDiameter() {
-    return 2 * this.radius;
+    getArea() {
+    return Math.PI * Math.pow(this.radius,2);
   }
   getCircumference() {
     return 2 * Math.PI * this.radius;
   }
-  getArea() {
-    return Math.PI * this.radius * this.radius;
+  getDiameter() {
+    return 2 * this.radius;
   }
+ 
 }
 const circle = new Circle(10);
-
-console.log('Diameter =', circle.getDiameter().toFixed(2));
+circle.getArea();
 circle.getCircumference();
 console.log('Circumference =', circle.getCircumference().toFixed(2));
-circle.getArea();
 console.log('Area =', circle.getArea().toFixed(2));
+console.log('Diameter =', circle.getDiameter().toFixed(2));
+
+
 
 
